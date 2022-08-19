fd0322fb1dfa2fa5cbab26a71c7ccc1abd6273c6
CastorMuxu <111074788+CastorMuxu@users.noreply.github.com>
2022-08-19 12:26:50 -0300

    Add files via upload

---
+++ exercicio python.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 12:18:38 2022
+
+@author: lab53"""
+
+
+
+funcionario = "felipe"
+idade = 23
+endereco ="santos"
+salario = 10000
+
+a = funcionario
+b = idade
+c = endereco
+d = salario
+
+print ("a = ",a)
+print ("b = ",b)
+print ("c = ",c)
+print ("d = ",d)
+
