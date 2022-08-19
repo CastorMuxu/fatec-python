2ae3d73e81cd20f3b26a66d6515d41c8006c3b11
CastorMuxu <111074788+CastorMuxu@users.noreply.github.com>
2022-08-19 12:17:30 -0300

    Add files via upload

---
+++ primeira aula de pythin.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+""" Primeira aula de python"""
+
+#atriubuindo valores do tipo inteiro  INT #
+a = 12
+b =-8
+
+# atribuindo valores do tipo STRING #
+c ="ciencia de dados"
+d ="!@%"
+e = ""
+f = "88"
+
+# aplicando a funçâo PRINT #
+print ("a = ",a)
+print ("b = ",b)
+print ("c = ",c)
+print ("d = ", d)
+print ("e = ",e)
+print ("f = ",f)
+
+# alterando de string para int#
+
+int (f)
+
+
+h = int (f)
+
+g = h - b
+print ("h - b =",g)
+
+
+
+
+
+
+
+
+       
+  
+        
+
+
+
