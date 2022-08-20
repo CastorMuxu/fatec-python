cf3642533432b4e46b6b629415a90ac05e223e6d
Felipe Feliciano <111074788+CastorMuxu@users.noreply.github.com>
2022-08-20 12:53:43 -0300

    Add files via upload

---
+++ primeira aula.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 12
+b = -8
+c = "felipe"
+d = "santos"
+e = "100"
+f =""
+ 
+
+
+print("a =", a) 
+print("b =", b)
+print("c =", c)
+print("d =", d)
+print("e =", e) 
+print("f =", f) 
+
+
+#conversao entre tipos#
+h = int (e)
+g = b + h
+print("g = (-8) + 100 = ",g)
+
+str (a)
+a = c
+print ("a: string = ", a)
+
+
+      
+         
+    
+
+     
+      
+     
