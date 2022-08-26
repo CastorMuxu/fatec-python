8cecf78c8c1047f36f3208ab79dc5463d06845f2
Felipe Feliciano <111074788+CastorMuxu@users.noreply.github.com>
2022-08-26 12:19:37 -0300

    Add files via upload

---
+++ segunda aula de python 2608.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 26 11:44:12 2022
+
+@author: lab54
+"""
+
+# dimensao de string#
+a =  "Felipe"
+print("dimensao da string a = ",len(a))
+print("dimensao da string com espaço = " ,len("a b"))
+print("dimensao de string vazia = ", len(""))
+
+#acesso a  caracteres de string#
+
+print("primeira letra de Felipe = " ,a[0])
+c = "a b"
+print("valor entra a e b = ",c[1])
+d = "abc"
+print("valor entre a e c = " ,d[1])
+
+#string concatenadas#
+e = " Feliciano Gonçalves Dias"
+print("nome do aluno", a + e)
+print("F" * 5)
+print("-*" * 10 + "-")
+
+
+
