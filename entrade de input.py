35e3478972f5d031c852895710d00b3413c3cfdf
Felipe Feliciano <111074788+CastorMuxu@users.noreply.github.com>
2022-08-26 12:48:15 -0300

    Add files via upload

---
+++ entrade de input.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 26 13:41:55 2022
+
+@author: lab54
+"""
+
+idade = int(input("idade: "))
+print(idade)
+nome = input("nome: ")
+print(nome)
+salario = float(input("digite o salario: R$ "))
+print(salario)
+
+print("idade maior",idade > 25)
+print("nome: ", nome== "felipe")
+print("salario: ", salario >=1550.50)
+
